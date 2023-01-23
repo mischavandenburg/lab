6c4f44dc03d286c2a5e84c51503f9918b942eb3d
Mischa <mischavandenburg@gmail.com>
2023-01-23 18:45:55 +0100

    added aks cluster terraform

---
+++ terraform/aks-cluster/main.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "mischa_aks" {
+  name     = "${var.prefix}-k8s-resources"
+  location = var.location
+}
+
+resource "azurerm_kubernetes_cluster" "mischa_aks" {
+  name                = "${var.prefix}-k8s"
+  location            = azurerm_resource_group.mischa_aks.location
+  resource_group_name = azurerm_resource_group.mischa_aks.name
+  dns_prefix          = "${var.prefix}-k8s"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
