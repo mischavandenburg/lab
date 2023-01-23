fa645f73c10e39cbcd3908da6244be06105af2e6
Mischa <mischavandenburg@gmail.com>
2023-01-23 19:48:47 +0100

    added registry

--- terraform/aks-cluster/main.tf
+++ terraform/aks-cluster/main.tf
@@ -12,6 +12,7 @@ resource "azurerm_kubernetes_cluster" "mischa_aks" {
   location            = azurerm_resource_group.mischa_aks.location
   resource_group_name = azurerm_resource_group.mischa_aks.name
   dns_prefix          = "${var.prefix}-k8s"
+  kubernetes_version  = "1.23.12"
 
   default_node_pool {
     name       = "default"
@@ -22,4 +23,18 @@ resource "azurerm_kubernetes_cluster" "mischa_aks" {
   identity {
     type = "SystemAssigned"
   }
+}
+
+resource "azurerm_container_registry" "mischa_aks" {
+  name                = "containerRegistry1"
+  resource_group_name = azurerm_resource_group.mischa_aks.name
+  location            = azurerm_resource_group.mischa_aks.location
+  sku                 = "Standard"
+}
+
+resource "azurerm_role_assignment" "mischa_aks" {
+  principal_id                     = azurerm_kubernetes_cluster.mischa_aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.mischa_aks.id
+  skip_service_principal_aad_check = true
 }
--- terraform/aks-cluster/outputs.tf
+++ terraform/aks-cluster/outputs.tf
@@ -1,23 +1,29 @@
 output "id" {
   value = azurerm_kubernetes_cluster.mischa_aks.id
+  sensitive = true
 }
 
 output "kube_config" {
   value = azurerm_kubernetes_cluster.mischa_aks.kube_config_raw
+  sensitive = true
 }
 
 output "client_key" {
   value = azurerm_kubernetes_cluster.mischa_aks.kube_config.0.client_key
+  sensitive = true
 }
 
 output "client_certificate" {
   value = azurerm_kubernetes_cluster.mischa_aks.kube_config.0.client_certificate
+  sensitive = true
 }
 
 output "cluster_ca_certificate" {
   value = azurerm_kubernetes_cluster.mischa_aks.kube_config.0.cluster_ca_certificate
+  sensitive = true
 }
 
 output "host" {
   value = azurerm_kubernetes_cluster.mischa_aks.kube_config.0.host
+  sensitive = true
 }
