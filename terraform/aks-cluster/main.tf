ea0c7a15e5dbc68c8a18579169c39b43e880af80
Mischa <mischavandenburg@gmail.com>
2023-01-25 10:40:47 +0100

    comment acr

--- terraform/aks-cluster/main.tf
+++ terraform/aks-cluster/main.tf
@@ -25,16 +25,16 @@ resource "azurerm_kubernetes_cluster" "mischa_aks" {
   }
 }
 
-resource "azurerm_container_registry" "mischa_aks" {
-  name                = "containerRegistry1"
-  resource_group_name = azurerm_resource_group.mischa_aks.name
-  location            = azurerm_resource_group.mischa_aks.location
-  sku                 = "Standard"
-}
+# resource "azurerm_container_registry" "mischa_aks" {
+#   name                = "${var.prefix}mischak8sregistry"
+#   resource_group_name = azurerm_resource_group.mischa_aks.name
+#   location            = azurerm_resource_group.mischa_aks.location
+#   sku                 = "Standard"
+# }
 
-resource "azurerm_role_assignment" "mischa_aks" {
-  principal_id                     = azurerm_kubernetes_cluster.mischa_aks.kubelet_identity[0].object_id
-  role_definition_name             = "AcrPull"
-  scope                            = azurerm_container_registry.mischa_aks.id
-  skip_service_principal_aad_check = true
-}
+# resource "azurerm_role_assignment" "mischa_aks" {
+#   principal_id                     = azurerm_kubernetes_cluster.mischa_aks.kubelet_identity[0].object_id
+#   role_definition_name             = "AcrPull"
+#   scope                            = azurerm_container_registry.mischa_aks.id
+#   skip_service_principal_aad_check = true
+# }
