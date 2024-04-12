9bad4dcb35873409ef94e60929ec476f5713b643
Mischa van den Burg <mischavandenburg@gmail.com>
2024-04-12 12:43:30 +0200

    start csharp learning

---
+++ csharp/hello-world/Program.cs
@@ -0,0 +1,8 @@
+﻿Console.WriteLine("Hello, World!");
+
+string firstFriend = " Maria  ";
+string secondFriend = "Scott  ";
+
+firstFriend = firstFriend.TrimStart();
+
+Console.WriteLine($"My friends are {firstFriend.TrimEnd()} and {secondFriend.TrimEnd()}");
