fb5e00f6527de52f25614b204d1a056dd371862b
Mischa van den Burg <mischavandenburg@gmail.com>
2024-04-14 10:11:07 +0200

    work: add first dotnet program

--- csharp/hello-world/Program.cs
+++ csharp/hello-world/Program.cs
@@ -1,8 +1,13 @@
 ﻿Console.WriteLine("Hello, World!");
 
+
 string firstFriend = " Maria  ";
 string secondFriend = "Scott  ";
 
 firstFriend = firstFriend.TrimStart();
 
 Console.WriteLine($"My friends are {firstFriend.TrimEnd()} and {secondFriend.TrimEnd()}");
+string friends= $"My friends are {firstFriend.TrimEnd()} and {secondFriend.TrimEnd()}";
+
+Console.WriteLine(friends.Replace("Scott", "John"));
+
