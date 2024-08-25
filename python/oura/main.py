571814a85d9558fb1b428cc609405f6669e56f50
Mischa <94125469+mischavandenburg@users.noreply.github.com>
2024-08-25 15:15:10 +0200

    python: oura

--- python/oura/main.py
+++ python/oura/main.py
@@ -7,15 +7,15 @@ token = os.getenv("OURA_TOKEN")
 
 # Sleep
 
-# url = "https://api.ouraring.com/v2/usercollection/sleep"
+url = "https://api.ouraring.com/v2/usercollection/sleep"
+params = {"start_date": "2024-08-21", "end_date": "2024-08-23"}
+headers = {"Authorization": f"Bearer {token}"}
+response = requests.request("GET", url, headers=headers, params=params)
+print(response.text)
+
+
+# url = "https://api.ouraring.com/v2/usercollection/heartrate"
 # params = {"start_date": "2024-08-18", "end_date": "2024-08-20"}
 # headers = {"Authorization": f"Bearer {token}"}
 # response = requests.request("GET", url, headers=headers, params=params)
 # print(response.text)
-
-
-url = "https://api.ouraring.com/v2/usercollection/heartrate"
-params = {"start_date": "2024-08-18", "end_date": "2024-08-20"}
-headers = {"Authorization": f"Bearer {token}"}
-response = requests.request("GET", url, headers=headers, params=params)
-print(response.text)
